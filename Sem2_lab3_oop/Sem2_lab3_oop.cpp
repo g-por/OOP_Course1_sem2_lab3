28f215e6faf794a8ab1aa23e7b8f3169c2bca974
logos <logos@DESKTOP-E25EKKO>
2023-05-12 10:49:33 +0300

    Зміни після правок викладача.

--- Sem2_lab3_oop/Sem2_lab3_oop.cpp
+++ Sem2_lab3_oop/Sem2_lab3_oop.cpp
@@ -5,150 +5,110 @@
 using namespace std;
 
 class Function {
-public:
+protected:
     double x;
     double y;
-    float result;
 
-    virtual double calculate() {
-        return result;
-    }
-    virtual void getResult() {
-        cout << "Result: " << result << endl;
-    }
+public:
+    Function() {
+        this->x = 0;
+        this->y = 0;
+    };
+    virtual double calculate() = 0;
 };
 
 class Multiplication : public Function {
 public:
-    Multiplication() {
+    Multiplication() : Function() {
         x = 7;
         y = 3.5;
     }
-    Multiplication(const Multiplication& sum) {
-        x = sum.x;
-        y = sum.y;
-
-    }
-    Multiplication(double x_input, double y_input) {
-        x = x_input;
-        y = y_input;
-    }
-
-    virtual double calculate() {
-        result = x * y;
-        return result;
+    double calculate() override {
+        return x * y;
     }
 };
 
 class Sum : public Function {
 public:
-    Sum() {
+    Sum() : Function() {
         x = 7;
         y = 2;
     }
-    virtual double calculate() {
-        result = x + y;
-        return result;
+    double calculate() override {
+        return x + y;
     }
 };
 
 class Menos : public Function {
 public:
-    Menos() {
+    Menos() : Function() {
         x = 19;
         y = 4;
     }
-    virtual double calculate() {
-        result = x - y;
-        return result;
+    double calculate() override {
+        return x - y;
     }
 };
 class ReverseMenos : public Function {
 public:
-    ReverseMenos() {
+    ReverseMenos() : Function() {
         x = 3;
         y = 2;
     }
-    virtual double calculate() {
-        result = y-x;
-        return result;
+    double calculate() override {
+        return y - x;
     }
 };
 
 class Division : public Function {
 public:
-    Division() {
+    Division() : Function() {
         x = 4;
         y = 5;
     }
-    virtual double calculate() {
-        result = x/y;
-        return result;
+    double calculate() override {
+        return x / y;
     }
 };
+
 class ReverseDivision : public Function {
 public:
-    ReverseDivision() {
+    ReverseDivision() : Function() {
         x = 13.75;
         y = 79;
     }
-    virtual double calculate() {
-        result = y/ x;
-        return result;
+    double calculate() override {
+        return y / x;
     }
 };
 class Container {
     list<Function*> functions;
 
 public:
-    void add(Multiplication* Multiplication_res) {
-        functions.push_back(Multiplication_res);
+    void add(Function* res) {
+        functions.push_back(res);
     }
 
-    void add(Sum* suma) {
-        functions.push_back(suma);
-    }
-
-    void add(Menos* Menos_res) {
-        functions.push_back(Menos_res);
-    }
-
-    void add(ReverseMenos* ReverseMenos_res) {
-        functions.push_back(ReverseMenos_res);
-    }
-
-    void add(Division* Divisions_res) {
-        functions.push_back(Divisions_res);
-    }
-
-    void add(ReverseDivision* ReverseDivision_res) {
-        functions.push_back(ReverseDivision_res);
-    }
-
-    void calculate() {
-        float res = 1;
-        for (auto it = functions.begin(); it != functions.end(); ++it) {
-            res *= (*it)->calculate();
-            res = abs(res);
-            (*it)->getResult();
+    double calculate() {
+        double res = 1;
+        for (Function* function : functions) {
+            res *= abs(function->calculate());
         }
-        cout << "The result of multiplying all functions: " << res;
+        return res;
     }
 };
 
 int main()
 {
     Container cont;
-    cont.add(new Multiplication(7, 4));
     cont.add(new Sum());
     cont.add(new Multiplication());
-
     cont.add(new Menos());
     cont.add(new ReverseMenos());
-
     cont.add(new Division());
     cont.add(new ReverseDivision());
-    cont.calculate();
+    cout << cont.calculate();
+
     return 0;
 }
 
